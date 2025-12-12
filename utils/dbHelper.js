78811be8c6a5c97962daa67e73d56ef4c3486edd
VitaSlice <idokdjdj@gmail.com>
2025-12-12 13:55:28 +0700

    Update dbHelper.js

--- utils/dbHelper.js
+++ utils/dbHelper.js
@@ -15,8 +15,8 @@ const SIGNATURE_PRODUCT_DATA = [
     // BLENDED SIGNATURE PRODUCTS (Original)
     
     // SLICED SIGNATURE PRODUCTS (New)
-    { objectId: 'sig-4', objectData: { name: 'Vita-Jasmine Series (Potongan)', description: 'Semangka, Lemon, Apel, melon, dengan kuah Teh Jasmine.', price: 30000, type: 'sliced', nutrients: JSON.stringify({ calories: 110, sugar: 18, vitaminC: 75, vitaminA: 80 }) } },
-    { objectId: 'sig-5', objectData: { name: 'Vita-Coco Series (Potongan)', description: 'Semangka, Lemon, Apel, melon, dengan sedikit Air Kelapa segar sebagai kuah.', price: 34000, type: 'sliced', nutrients: JSON.stringify({ calories: 250, sugar: 25, vitaminC: 15, vitaminA: 10 }) } },
+    { objectId: 'sig-4', objectData: { name: 'Vita-Jasmine Series (Potongan)', description: 'Semangka, Lemon, Apel, melon, dengan kuah Teh Jasmine.', price: 20000, type: 'sliced', nutrients: JSON.stringify({ calories: 110, sugar: 18, vitaminC: 75, vitaminA: 80 }) } },
+    { objectId: 'sig-5', objectData: { name: 'Vita-Coco Series (Potongan)', description: 'Semangka, Lemon, Apel, melon, dengan sedikit Air Kelapa segar sebagai kuah.', price: 20000, type: 'sliced', nutrients: JSON.stringify({ calories: 250, sugar: 25, vitaminC: 15, vitaminA: 10 }) } },
 ];
 
 // --- SIMULASI PEMBANTU DATABASE (Non-Persistent) ---
